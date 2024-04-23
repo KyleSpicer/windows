1d61ef963dbe334a8731790168d23a9242bd8030
Kyle <kyle.d.spicer.mil@army.mil>
2024-04-23 07:55:34 -0700

    Update make debug recipe

--- cmake/source/src/helpers.c
+++ cmake/source/src/helpers.c
@@ -1,10 +1,11 @@
+#include <Windows.h>
 #include <stdio.h>
 #include <wchar.h>
-#include <Windows.h>
 
 #include "../include/helpers.h"
 
-void custom_print(void)
+void
+custom_print (void)
 {
     wprintf(L"Hello World!!!\n");
 }
--- cmake/source/src/main.c
+++ cmake/source/src/main.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <Windows.h>
+#include <stdio.h>
 #include <wchar.h>
 
 #include "../include/helpers.h"
 
-int wmain(void)
+int
+wmain (void)
 {
     custom_print();
     return 0;
