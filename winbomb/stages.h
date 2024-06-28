625a2c48cf71e3c5a089336690025bf921fb02ab
Jacob Hitchcox <jacob.d.hitchcox.mil@army.mil>
2024-06-28 19:02:33 -0400

    remove comment

--- winbomb/stages.h
+++ winbomb/stages.h
@@ -5,7 +5,7 @@
 #define ARRAY_LEN 100000
 #define INPUTLEN 128
 #define WORDY "%127s"
-// define a 31 byte string
+
 #define LONG_STR "This is my long secret string."
 CHAR un[INPUTLEN];
 CHAR nd[INPUTLEN];
